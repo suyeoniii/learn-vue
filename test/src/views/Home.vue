1facabc9196c6042f9b8b0e2eeefde21dab15dbc
ssy4230@naver.com <ssy4230@naver.com>
2021-01-18 09:13:17 +0900

    table

--- test/src/views/Home.vue
+++ test/src/views/Home.vue
@@ -5,9 +5,17 @@
     <button type="button" @click="setData">set</button>
     <button type="button" @click="getData">button</button>
 
-    <select class="form-control" v-model="region">
-      <option :key="i" value="d.v" v-for="(d, i) in options">{{ d.t }}</option>
+    <select class="form-control" v-model="region" @change="changeRegion">
+      <option :key="i" :value="d.v" v-for="(d, i) in options">{{ d.t }}</option>
     </select>
+
+    <!-- 유사한걸로 v-show 잇는데 얘는 일단 렌더링하고 안보여주는거, v-if는 렌더링을 아에 안함 -->
+    <table class="table table-bordered" v-if="tableShow">
+      <tr :key="i" v-for="(d, i) in options">
+        <td>{{ d.v }}</td>
+        <td>{{ d.t }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 <script>
@@ -22,6 +30,7 @@ export default {
         { v: "B", t: "Busan" },
       ],
       region: "J",
+      tableShow: false,
     };
   },
   watch: {
@@ -35,6 +44,9 @@ export default {
     },
     setData() {
       this.input = "data";
+    },
+    changeRegion() {
+      alert(this.region);
     },
   },
 };

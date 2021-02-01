bf009bd118ae591b4490739b382468828d41d11d
ssy4230@naver.com <ssy4230@naver.com>
2021-02-02 00:30:52 +0900

    about

--- test/src/views/About.vue
+++ test/src/views/About.vue
@@ -1,8 +1,22 @@
 <template>
   <div>
-    <h1>About Page</h1>
+    <h1>About {{ name }}</h1>
+    <input type="text" v-model="input" />
+    <button type="button" @click="getData">get</button>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      name: "subari",
+      input: "text",
+    };
+  },
+  methods: {
+    getData() {
+      alert(this.input);
+    },
+  },
+};
 </script>
